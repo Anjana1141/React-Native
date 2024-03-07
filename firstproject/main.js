b9e45ef069f5ef138073729545873fe69814ea46
Anjana1141 <anjanaicse@gmail.com>
2024-03-07 18:48:27 +0530

    first Project

---
+++ firstproject/main.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ * @param {Boolean} number1 
+ * @param {Number} number2 
+ * @returns
+ */
+
+function add(number1 , number2){
+
+    return {
+        number1 : number1,
+        number2 : number2
+    }
+}
